7cc2fc6243e3f14297ec169a2ac03bfa27280bc7
Muhammad Naveed <epicplayz.147@gmail.com>
2023-04-11 13:08:47 +0500

    for loop
    Js using for loop

---
+++ app.js
@@ -0,0 +1,5 @@
+let tableNumber = +prompt("Enter Your Table Number");
+let tableLength = +prompt("Enter Your Table Length");
+for (let i = 1; i <= tableLength; i++ ){
+    document.write(tableNumber +" X " + i + " = " + tableNumber*i + "<br>")
+};
